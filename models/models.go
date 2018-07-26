04a3794cb1a25dfa2af64a693f7dd56bd0906c39
Flynn <perpetualy@outlook.com>
2018-07-26 09:26:34 +0800

    格式化MODEL

--- models/models.go
+++ models/models.go
@@ -2,49 +2,49 @@ package models
 
 //Config ini配置文件
 type Config struct {
-	DBAddress     string `ini:"DBAddress"`
-	ServerAddress string `ini:"ServerAddress"`
-	ServerPort    string `ini:"ServerPort"`
-	SSLCrtFile    string `ini:"SSLCrtFile"`
-	SSLKeyFile    string `ini:"SSLKeyFile"`
+	DBAddress     	string `ini:"DBAddress"`
+	ServerAddress 	string `ini:"ServerAddress"`
+	ServerPort    	string `ini:"ServerPort"`
+	SSLCrtFile    	string `ini:"SSLCrtFile"`
+	SSLKeyFile    	string `ini:"SSLKeyFile"`
 }
 
 //User 用户信息
 type User struct {
-	Key      string `json:"_key,omitempty"`
-	User     string `json:"user" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Address  string `json:"address" binding:"required"`
-	FullName string `json:"fullname" binding:"required"`
-	Phone    string `json:"phone" binding:"required"`
-	Company  string `json:"company" binding:"required"`
+	Key      		string `json:"_key,omitempty"`
+	User     		string `json:"user" binding:"required"`
+	Password 		string `json:"password" binding:"required"`
+	Email    		string `json:"email" binding:"required"`
+	Address  		string `json:"address" binding:"required"`
+	FullName 		string `json:"fullname" binding:"required"`
+	Phone    		string `json:"phone" binding:"required"`
+	Company  		string `json:"company" binding:"required"`
 
-	Mac       string `json:"mac" binding:"required"`
-	Disk0     string `json:"disk0" binding:"required"`
-	Salt      int64  `json:"salt" binding:"required"`		//暂时无用 强制设定为2
-	Duration  int64  `json:"duration" binding:"required"`
-	RegDate   string `json:"regdate"`
-	ExpDate   string `json:"expdate"`
-	Activated int64  `json:"activated"`
+	Mac       		string `json:"mac" binding:"required"`
+	Disk0     		string `json:"disk0" binding:"required"`
+	Salt      		int64  `json:"salt" binding:"required"`		//暂时无用 强制设定为2
+	Duration  		int64  `json:"duration" binding:"required"`
+	RegDate   		string `json:"regdate"`
+	ExpDate   		string `json:"expdate"`
+	Activated 		int64  `json:"activated"`
 }
 
 //MAC 注册的MAC
 type MAC struct {
-	Key     string `json:"_key,omitempty"`
-	UserKey string `json:"userkey"`
+	Key     		string `json:"_key,omitempty"`
+	UserKey 		string `json:"userkey"`
 }
 
 //Disk0 注册的Disk0
 type Disk0 struct {
-	Key     string `json:"_key,omitempty"`
-	UserKey string `json:"userkey"`
+	Key     		string `json:"_key,omitempty"`
+	UserKey 		string `json:"userkey"`
 }
 
 //Update 版本更新
 type Update struct {
-	Key     string `json:"_key,omitempty"`
-	Version string `json:"version"`
-	Info    string `json:"info"`
-	RelDate string `json:"reldate"`
+	Key     		string `json:"_key,omitempty"`
+	Version 		string `json:"version"`
+	Info    		string `json:"info"`
+	RelDate 		string `json:"reldate"`
 }
