76ccbbc7d1e7359ea2cbf5b1b908cf641cbb67b0
perpetualy <perpetualy@outlook.com>
2018-08-09 15:08:01 +0800

    更新脚本

--- srvupdate_linux.sh
+++ srvupdate_linux.sh
@@ -1,1 +1,6 @@
+go install stonesrv
+cp /home/flynnyal/application/go/bin/stonesrv /home/flynnyal/application/go/src/stonesrv/package
+cp /home/flynnyal/application/go/src/stonesrv/language /home/flynnyal/application/go/src/stonesrv/package
+cp /home/flynnyal/application/go/src/stonesrv/updates /home/flynnyal/application/go/src/stonesrv/package
+rm /home/flynnyal/application/go/src/stonesrv/package/language/*.go
 scp -r /home/flynnyal/application/go/src/stonesrv/package root@202.182.127.17:/root/application/go/src/stonesrv
