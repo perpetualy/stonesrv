13e9f5385010ac948eaedc4ae0f8b4c0ecf4fca6
FlynnYal <perpetualy@outlook.com>
2018-09-09 13:06:35 +0800

    linux服务配置

--- srvupdate_linux.sh
+++ srvupdate_linux.sh
@@ -2,6 +2,8 @@ go install stonesrv
 cp /home/fy/application/go/bin/stonesrv /home/fy/application/go/src/stonesrv/package
 cp -r /home/fy/application/go/src/stonesrv/language /home/fy/application/go/src/stonesrv/package
 cp -r /home/fy/application/go/src/stonesrv/updates /home/fy/application/go/src/stonesrv/package
+cp -r /home/fy/application/go/src/stonesrv/conf /home/fy/application/go/src/stonesrv/package
 rm /home/fy/application/go/src/stonesrv/package/language/*.go
+rm /home/fy/application/go/src/stonesrv/package/conf/*.go
 scp -r /home/fy/application/go/src/stonesrv/package root@202.182.127.17:/root/application/go/src/stonesrv
 
